8a8fcde2fae1eee37b5d193d05b37246dc9d2059
Бураков Иван <drakon1225@gmail.com>
2025-12-02 12:42:32 +0300

    Nav bar creation and styling

--- app.py
+++ app.py
@@ -3,8 +3,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Мой сайт табов работает!"
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/menu')
+@app.route('/home/menu')
+def menu_page():
+    return render_template('menu.html')
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
